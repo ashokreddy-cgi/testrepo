774f5a39d28213e3ad1d7b605ae03476f699c2fc
Prathap <ashokreddy.cgi@gmail.com>
2023-03-26 14:19:01 +0530

    main branch commit

--- testproject/src/testproject/HelloWorld.java
+++ testproject/src/testproject/HelloWorld.java
@@ -5,16 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld... !!!");
 		//commit
-		System.out.println("master commit-1");
-		System.out.println("master commit-2");
-		System.out.println("master commit-3");
-		System.out.println("master commit-4");
-		System.out.println("master commit-5");
-		System.out.println("master commit-6");
-		System.out.println("master commit-7");
-		System.out.println("master commit-8");
-		System.out.println("master commit-9");
-		System.out.println("master commit-10");
+
 
 
 	}
