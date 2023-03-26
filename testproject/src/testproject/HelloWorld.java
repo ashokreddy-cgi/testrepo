e55772958de5ee85f51a757199a0153bce1834dd
Prathap <ashokreddy.cgi@gmail.com>
2023-03-26 10:34:00 +0530

    main branch commit

--- testproject/src/testproject/HelloWorld.java
+++ testproject/src/testproject/HelloWorld.java
@@ -5,10 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld... !!!");
 		//commit
-		System.out.println("commit message");
-		System.out.println("main branch commit");
-		System.out.println("main branch commit-1");
-		System.out.println("main branch commit-2")
+
 
 	}
 
